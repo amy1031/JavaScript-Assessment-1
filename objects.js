bebd87508b5d7690b546194f898b1fc58c67ebe9
Amy Roche <amy.roche31@gmail.com>
2017-04-25 21:47:37 -0600

    replace values in object

--- objects.js
+++ objects.js
@@ -1,9 +1,16 @@
-//1. Using dot and bracket notation modify the existing object values to replace the information with your own
+//1. Using dot and bracket notation modify the existing object values 
+// to replace the information with your own
 
 var me = {
 	'name': 'Jake Overall',
 	'age': 29,
 	'hair color': 'brown'
 };
+
+me.name = "Amy";
+me.age = 27;
+me['hair color'] = "Light Brown";
+
+console.log(me);
 
 //2. Iterate over the object to console.log the propery or key names. 
