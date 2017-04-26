46c6e4e7d5cf149c64111bd58fd5016f096a7e1c
Amy Roche <amy.roche31@gmail.com>
2017-04-25 22:15:54 -0600

    add mentors array

--- arrays.js
+++ arrays.js
@@ -41,5 +41,16 @@ contacts.push(jake, matt, chris, tony, andrew, stefan);
 console.log(contacts);
 
 
-//3. Woops after adding all of those people to the same contacts list you realized you need a list just the mentors. Create a new variable named mentors populate it using contacts array. 
 
+//3. Woops after adding all of those people to the same contacts list 
+// you realized you need a list just the mentors. Create a new variable 
+// named mentors populate it using contacts array. 
+
+var mentors = [];
+
+for (var i = 0; i < contacts.length; i++){
+	var contact = contacts[i];
+	if (contact.title == "mentor") {
+		mentors.push(contact); 
+	} console.log(mentors);
+}
