e1858e31f9fea1832596eeb5c118eb30448aaebe
Amy Roche <amy.roche31@gmail.com>
2017-04-25 21:20:22 -0600

    provide sum in numbers

--- numbers.js
+++ numbers.js
@@ -2,6 +2,15 @@
 var num10 = 10;
 var string8 = '8';
 var one = 1;
+
+function findSum(num10, string8, one) {
+    string8 = parseInt(string8);
+    var sum = num10 + string8 + one;
+    console.log(sum);
+}
+
+findSum(num10, string8, one);
+
 //2. write a loop that will log only numbers divisible by 3 between 20 - 100
 
 
