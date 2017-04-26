8fc54d2d48aa24de7da4ec5e41555143c5842da3
Amy Roche <amy.roche31@gmail.com>
2017-04-25 22:29:56 -0600

    hyphenate crayons

--- real-world-application.js
+++ real-world-application.js
@@ -1,7 +1,10 @@
-/*Imagine you work for Crayola and your boss just told you that the Crayola1990 API needs to have a url slug generated for all of the colors names.
+/*Imagine you work for Crayola and your boss just told you that the 
+Crayola1990 API needs to have a url slug generated for all of the colors names.
 * To do this you need to lowercase and hyphenate each color in the following array.
-* You can not edit the values in the array by hand as your boss is wanting a loop that can be applied to any set of crayola boxes in the future
+* You can not edit the values in the array by hand as your boss is wanting a 
+loop that can be applied to any set of crayola boxes in the future
 */
+
 var crayola1990 = [
 	'Scarlet',
 	'Sunset Orange',
@@ -58,10 +61,24 @@ var crayola1990 = [
 	'Antique Brass'
 ];
 
-//create a loop that will iterate over each item in the array and then use the String methods to modify the value at the current position in the array
+function editCrayons(crayola1990) {
+ for (var i = 0; i < crayola1990.length; i++) {
+	 var crayon = crayola1990[i].toLowerCase();
+	 var replace = crayon.replace(/ /g, "-");
+	 console.log(replace);
+ 	}
+};
+
+editCrayons(crayola1990);
+
+//create a loop that will iterate over each item in the array and then use 
+// the String methods to modify the value at the current position in the array
 
 /***** HINTS *******
- * remember strings are immutable but the value of a variable can be adjusted as often as needed.
- * take advantage of the methods available on stings http://www.w3schools.com/js/js_string_methods.asp
- * there are multiple ways to do something like this but you might consider using split and join
+ * remember strings are immutable but the value of a variable can be adjusted 
+ * as often as needed.
+ * take advantage of the methods available on 
+ * stings http://www.w3schools.com/js/js_string_methods.asp
+ * there are multiple ways to do something like this but you might consider 
+ * using split and join
  */ 
